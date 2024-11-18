9d7c8afa5226646bc750064566db0ab49c665897
dmilovi <daria.milovi05@gmail.com>
2024-11-18 17:46:18 +0100

    Update pgcd.c

--- pgcd.c
+++ pgcd.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int ac, char av[]) //ищет наибольший общий делитель
+int main(int ac, char *av[]) //ищет наибольший общий делитель
 {
     int nbr1; //первое число
     int nbr2; //второе число
